d1fd8c3371bd4cb94f28f1825dde773fd214e241
Hatem Nassrat <hnassrat@gmail.com>
2012-09-29 18:31:52 -0300

    More changes to app ... testing in progress.

--- app.js
+++ app.js
@@ -20,8 +20,8 @@ app.configure(function(){
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
-  app.use(express.cookieParser('your secret here'));
-  app.use(express.session());
+//   app.use(express.cookieParser('your secret here'));
+//   app.use(express.session());
   app.use(app.router);
   app.use(express.static(path.join(__dirname, 'public')));
 });
@@ -36,15 +36,34 @@ app.get('/st', routes.st);
 app.get('/users', user.list);
 
 var http_app = http.createServer(app);
+var clients = {};
 
 http_app.listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
 
-var sio = io.listen(http_app);
+var sio = io.listen(http_app, { log: false });
+// sio.set('log level', 2); // remove the debug messages
 sio.sockets.on('connection', function (socket) {
     console.log('A socket connected!');
-    socket.on('video', function (data) {
-      console.log(data);
+    socket.on('user', function(data) {
+      clients[data.userid] = socket;
+      for (var client in clients) {
+        var s = clients[client];
+        if (true || s != socket) {
+          s.emit('user', data);
+        }
+      }
     });
+    socket.on('video', function (data) {
+      // console.log(data);
+      for (var client in clients) {
+        // broadcast to all sockets listening
+        var s = clients[client];
+        if (true || s != socket) {
+          s.emit(client, data);
+        }
+      }
+    });
+    
 });
